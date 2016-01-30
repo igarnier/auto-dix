1588fc4ef452abe80b8ae979284fc0bf6b7305cc
Ilias <ilias.gar@gmail.com>
2016-01-30 17:01:57 +0100

    -

--- graph.mli
+++
@@ -1,7 +0,0 @@
-module Make : functor (VL : Autotools.PrintableOrderedType) ->
-              functor (EL : Autotools.PrintableOrderedType) ->
-              GraphSig.S
-              with type V.L.t = VL.t
-               and type E.L.t = EL.t
-
-
